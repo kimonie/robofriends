b283fbffcdc3c1d57ce3eb3a82feb45320276e9e
Seuun <seunkikiowo@gmail.com>
2019-08-15 15:03:02 +0100

    react startup test with hello world and tachyons for styling

---
+++ src/Hello.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './Hello.css';
+
+
+class Hello extends Component {
+    render() {
+        return (
+            <div className="f1 tc">
+                <h1>Hello</h1>
+                <p>Welcome to react JS</p>
+            </div>
+        )
+        
+    }
+}
+
+export default Hello;
