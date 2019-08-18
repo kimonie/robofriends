cbb78a6dfd0d3464336dcfda14b31b8b97830d7b
Seuun <seunkikiowo@gmail.com>
2019-08-18 22:46:11 +0100

    Updates

---
+++ precache-manifest.d29a2caee39a95d8002ca3fe822fba0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "621b1ee38afd0e8699dff797b4ca0521",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "fbf12e553abb5a6b377e",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "43cd15539c0284c2b76e",
+    "url": "/robofriends/static/css/main.18c7b381.chunk.css"
+  },
+  {
+    "revision": "fbf12e553abb5a6b377e",
+    "url": "/robofriends/static/js/2.02c0cd4f.chunk.js"
+  },
+  {
+    "revision": "43cd15539c0284c2b76e",
+    "url": "/robofriends/static/js/main.95195eb0.chunk.js"
+  },
+  {
+    "revision": "fa64f21900e25e9ebf3b",
+    "url": "/robofriends/static/js/runtime~main.2c7325ee.js"
+  }
+]);
