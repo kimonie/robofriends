78e1a9323cb82d3677b1f0e2eb276fab6372c39d
Seuun <seunkikiowo@gmail.com>
2019-08-18 22:11:05 +0100

    binding and header

--- src/App.js
+++ src/App.js
@@ -12,12 +12,18 @@ class Card extends Component {
             monster: [],
             searchField: ''
         }
+
+        this.handleChange = this.handleChange.bind(this);
     }
 
     componentDidMount() {
         fetch('https://jsonplaceholder.typicode.com/users')
         .then(Response => Response.json())
         .then(users => this.setState({ monster : users }));
+    }
+
+    handleChange = (e) => {
+        this.setState({ searchField: e.target.value });   
     }
 
     render() {
@@ -28,14 +34,14 @@ class Card extends Component {
 
         return (
             <div className='App'>
+                <h1>ROBOFRIENDS</h1>
                 <SearchBox 
                     placeholder="Search Monsters" 
-                    handleChange={ e => this.setState({ searchField: e.target.value })} />
+                    handleChange={ this.handleChange } />
                 <CardList monster={ filteredMonsters } />
             </div>
         )
     }
 }
-
 
 export default Card;
